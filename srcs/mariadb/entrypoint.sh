9df7163677523dcb0911d7f26aa0cb6436fb4ab8
Romao Quilamiquiza <engquilamiquiza@gmail.com>
2025-08-29 17:17:57 +0100

    error on mariadb

--- srcs/mariadb/entrypoint.sh
+++ srcs/mariadb/entrypoint.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
 
+mysqld_safe --user=mysql --skip-networking --datadir=/var/lib/mysql &
+
+while ! mysqladmin ping -h"localhost" --silent; do
+    sleep 1
+done
 
 mysql -u root <<EOSQL
-ALTER USER 'root'@'rquilami.42.fr' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 DROP DATABASE IF EXISTS test;
 FLUSH PRIVILEGES;
 EOSQL
+
+/docker-entrypoint-initdb.d/init_db.sh
+
+mysqladmin -u root -p"$DB_ROOT_PASSWORD" shutdown
+
+# Iniciar como processo principal (PID 1)
+exec mysqld --user=mysql --datadir=/var/lib/mysql
--- srcs/wordpress/setup_wordpress.sh
+++ srcs/wordpress/setup_wordpress.sh
@@ -1,25 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-while ! mariadb -h"${MARIADB_HOST}" -u"${MARIADB_USER}" -p"$MARIADB_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
-    echo "Waiting for MariaDB to be ready..."
-    sleep 5
+#apt-get update && apt-get install -y mariadb-client
+while ! mariadb -h"${DB_HOST}" -u"${DB_USER}" -p"${DB_PASSWORD}"; do
+     echo "Waiting for MariaDB to be ready..."
+     sleep 5
 done
 if [ ! -f /var/www/html/wp-config.php ]; then
-    echo "WordPress not installed. Installing..."    if ! wp core download --allow-root --locale=pt_BR --path=/var/www/html; then
+    echo "WordPress not installed. Installing..."
+    if ! wp core download --allow-root --locale=pt_BR --path=/var/www/html; then
         echo "Error: Failed to download WordPress."
         exit 1
-    fi    if ! wp config create --dbname="${MARIADB_DATABASE}" --dbuser="${MARIADB_USER}" --dbpass="${MARIADB_PASSWORD}" --dbhost="${MARIADB_HOST}" --allow-root --path=/var/www/html; then
+    fi    if ! wp config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="${DB_HOST}" --allow-root --path=/var/www/html; then
         echo "Error: Failed to create wp-config.php."
         exit 1
-    fi    if ! wp core install --url="${DOMAIN_NAME}" --title="${WORDPRESS_DB_TITLE}" --admin_user="${WORDPRESS_DB_USER_ADMIN}" --admin_password="${WORDPRESS_DB_PASSWORD_ADMIN}" --admin_email="${WORDPRESS_DB_EMAIL_ADMIN}" --skip-email --allow-root --path=/var/www/html; then
+    fi    if ! wp core install --url="${DOMAIN_NAME}" --title="${WD_TITLE}" --admin_user="${WD_USER_ADMIN}" --admin_password="${WD_PASSWORD_ADMIN}" --admin_email="${WD_EMAIL_ADMIN}" --skip-email --allow-root --path=/var/www/html; then
         echo "Error: Failed to install WordPress."
         exit 1
-    fi    if ! wp user create "${WORDPRESS_DB_USER}" "${WORDPRESS_DB_EMAIL}" --role=author --user_pass="${WORDPRESS_DB_PASSWORD}" --allow-root --path=/var/www/html; then
+    fi    if ! wp user create "${WD_USER}" "${WD_EMAIL}" --role=author --user_pass="${WD_PASSWORD}" --allow-root --path=/var/www/html; then
         echo "Error: Failed to create WordPress user."
         exit 1
     fi
     echo "WordPress installed successfully."
 else
-    echo "WordPress already installed."
+     echo "WordPress already installed."
 fi
 
