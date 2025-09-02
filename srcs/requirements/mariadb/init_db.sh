e54e75ae74d2ada3a69f54145225728b2b114fc8
Romao Quilamiquiza <engquilamiquiza@gmail.com>
2025-09-02 17:21:46 +0100

    secrets

--- srcs/requirements/mariadb/init_db.sh
+++ srcs/requirements/mariadb/init_db.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DB_PASSWORD=sed -n '1p' /etc/security/db_password.txt
+DB_ROOT_PASSWORD=sed -n '1p' /etc/security/db_root_password.txt
+DB_USER=sed -n 
+
 while ! mysqladmin ping --silent; do
     sleep 1
 done
